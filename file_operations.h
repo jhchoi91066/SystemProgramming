c401e4a7fa94a46e9ad8bda7740d5c728828dabe
최진호 <127467867+jhchoi91066@users.noreply.github.com>
2024-06-06 17:49:15 +0900

    Delete file_operations.h

--- file_operations.h
+++
@@ -1,9 +0,0 @@
-#ifndef FILE_OPERATIONS_H
-#define FILE_OPERATIONS_H
-
-#include <stdio.h>
-
-void create_file(const char *filename, const char *content);
-void read_file(const char *filename);
-
-#endif
